fdfe9de224d015a983d6b47e29bb9a92eae3b966
Christian <christian.balazs@yahoo.com>
2021-11-10 11:51:21 +0000

    Re: MileTrack - Constructor class and Service interface set up

--- MileTrack/src/main/java/com/miletrack/main/service/MileTrackService.java
+++ MileTrack/src/main/java/com/miletrack/main/service/MileTrackService.java
@@ -1,6 +1,30 @@
 package com.miletrack.main.service;
 
-//Role: Interface to define CRUD methods used for communication with front end 
-public class MileTrackService {
+import java.util.List;
 
+import com.miletrack.main.domain.MileTrack;
+
+
+
+//Role: Interface to define CRUD methods used for communication with front end 
+
+public interface MileTrackService {
+
+	//Create Journey(object) method
+	MileTrack createJourney(MileTrack newMileTrack);
+	
+	//Read all journeys method
+	List<MileTrack> getAllJourneys();
+	
+	//Read a specific journey
+	MileTrack getJourney(Integer id);
+	
+	//Update the record for one journey with new object variables
+	MileTrack replaceJourney(Integer id, MileTrack newMileTrack);
+			
+	//Delete a journey from DB
+	boolean removeJourney(Integer id);
+	
+	
+	
 }
